51cce1006707d87880e292ea59806b0ecf2a8d60
Nathan Blake <developer@blake-ink.com>
2021-03-13 11:52:23 -0800

    Add DrinkClass, MovieClass, and RecommendationEngine js classes

---
+++ public/scripts/DrinkClass.js
@@ -0,0 +1,18 @@
+class Drink {
+	constructor(name){
+		this.name = name;
+		this.ingredients = [];
+	}
+	
+	get ingredients(){
+		return this.ingredients;
+	}
+
+	set tyep(t){
+		this.type = t;
+	}
+
+	addIngredient(ingredient){
+		this.ingredients;
+	}
+}
